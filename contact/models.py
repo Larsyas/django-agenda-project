78226cb2747e1e2d923b2e870d44e2d98cfde013
Larsyas <cjunior036@gmail.com>
2024-03-25 11:59:44 -0300

    Added 1000 fake contacts for testing

--- contact/models.py
+++ contact/models.py
@@ -1,14 +1,28 @@
 from email.policy import default
+from operator import truediv
+from unicodedata import category
 from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Contact(models.Model):
@@ -18,6 +32,20 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    pictures = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
